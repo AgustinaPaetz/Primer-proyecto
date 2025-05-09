be95b105e23e5d772717c9e8767a8d8b983f6317
Agustina <paetzagustina@gmail.com>
2025-05-08 21:33:03 -0300

    ejercicio sumas

--- codigo.js
+++ codigo.js
@@ -1,5 +1,12 @@
-var x = 1; 
-        while ( x < 10) {
-            x = x + 1;
-            console. log (x);
-        }
+function mostrarDato () {
+    var input=document.getElementById("input dato")
+    var parrafo=document.getElementById("resultado");
+var total=document.getElementById("total");
+parrafo.innertext="el dato ingresado es:"+ input.value +"\n";
+suma+=parseInt(input.value);
+if(suma>10)
+total.innertext=suma;
+else 
+total.innertext= 'la suma es menor o igual a 10'
+
+}
